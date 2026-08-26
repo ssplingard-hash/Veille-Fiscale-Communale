7c9c3b8c2440cf7cf27760aa423c2a5b4d5d24f6
ssplingard-hash <s.splingard@gmail.com>
2026-08-26 15:46:20 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import MunicipalityDetail from './pages/MunicipalityDetail';
