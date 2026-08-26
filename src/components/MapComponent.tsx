f162dec137cce3c040ecdb5e6dd394f068990bfc
ssplingard-hash <s.splingard@gmail.com>
2026-08-26 17:30:53 +0200

    Update MapComponent.tsx

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -4,7 +4,7 @@ import { Plus, Minus } from "lucide-react";
 import { municipalities } from '../data/municipalities';
 import { useNavigate } from 'react-router-dom';
 
-const geoUrl = `${import.meta.env.BASE_URL}b.json`;
+const geoUrl = `${import.meta.env.BASE_URL}b.geojson`;
 
 interface MapProps {
   mapType: 'IPP' | 'PRI' | 'TAXES';
